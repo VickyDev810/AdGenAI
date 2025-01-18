6cf9423ad2cfdea0c2f7f0754d432d3a1f06e7ca
VickyDev810 <veervikram810@gmail.com>
2025-01-18 11:30:07 +0530

    work 11:30am

--- client/src/pages/homepage.tsx
+++ client/src/pages/homepage.tsx
@@ -45,24 +45,6 @@ function HomePage() {
     };
 
     return (
-<<<<<<< HEAD
-        <div className="flex h-screen bg-">
-            <div className="w-[40%] p-10 bg-black">
-                <h1 className="text-4xl font-extrabold text-white mb-5">Summer Jams Concert Series</h1>
-                <input
-                    type="text"
-                    className="border border-white h-52 rounded-lg p-4 w-full mb-4 bg-white"
-                    placeholder="Enter your prompt"
-                />
-               
-                <div className="flex items-center mb-4">
-                    <button className="bg-blue-600 text-white rounded-lg p-2 mr-2 hover:bg-blue-700">
-                        <i className="fas fa-paperclip"></i> {/* Attachment icon */}
-                        <i className="fas fa-paperclip"></i> {/* Attachment icon */}
-                    </button>
-                    <button className="bg-green-500 text-white rounded-lg p-2">
-                        <i className="fas fa-bolt"></i> {/* Generate icon */}
-=======
         <div className="flex flex-col h-screen" style={{ fontFamily: 'Poppins' }}>
             <Header />
             <div className="flex-grow flex">
@@ -76,7 +58,6 @@ function HomePage() {
                     <button className="flex gap-3 bg-violet text-white rounded-lg p-5 mb-10 justify-center">
                         Generate
                         <img src="arrow.png" className="h-5 w-5" />
->>>>>>> 5cf70b2c577e6b9c63083051d5fce5554ab1b34b
                     </button>
                     <div className="flex justify-center mb-4 w-full">
                         <button className="bg-signinpurple rounded-lg p-2 mx-2 text-white w-full h-20">
@@ -87,18 +68,6 @@ function HomePage() {
                         </button>
                     </div>
                 </div>
-<<<<<<< HEAD
-                <button className="bg-yellow-500 text-white rounded-lg p-2 mb-4">Generate</button>
-                <div className="flex justify-center mb-4">
-                    <button className="bg-gray-300 rounded-full p-2 mx-2">Text to Image</button>
-                    <button className="bg-gray-300 rounded-full p-2 mx-2">Image to Image</button>
-                    <button className="bg-gray-300 rounded-full p-2 mx-2">Text to Video</button>
-                </div>
-            </div>
-            <div className="w-[60%] p-2">
-                <div className="border border-gray-300 rounded-lg h-full">
-                    {/* Images will be displayed here */}
-=======
                 <div className="w-[60%] p-10 bg-gray-200 overflow-hidden relative">
                     <div className="relative w-full h-full">
                         {questions.map((question, index) => (
@@ -198,7 +167,6 @@ function HomePage() {
                             </div>
                         ))}
                     </div>
->>>>>>> 5cf70b2c577e6b9c63083051d5fce5554ab1b34b
                 </div>
             </div>
             <Footer />
