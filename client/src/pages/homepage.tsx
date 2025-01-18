34d09ad4e04d07edb717852d87f5bf88e12faff4
VickyDev810 <veervikram810@gmail.com>
2025-01-18 11:08:15 +0530

    merging

--- client/src/pages/homepage.tsx
+++ client/src/pages/homepage.tsx
@@ -1,13 +1,14 @@
 function HomePage() {
     return (
-        <div className="flex h-screen bg-slate-400">
-            <div className="w-1/2 p-10">
+        <div className="flex h-screen bg-">
+            <div className="w-[40%] p-10 bg-black">
                 <h1 className="text-4xl font-extrabold text-white mb-5">Summer Jams Concert Series</h1>
                 <input
                     type="text"
-                    className="border border-white rounded-lg p-4 w-full mb-4 bg-white"
+                    className="border border-white h-52 rounded-lg p-4 w-full mb-4 bg-white"
                     placeholder="Enter your prompt"
                 />
+               
                 <div className="flex items-center mb-4">
                     <button className="bg-blue-600 text-white rounded-lg p-2 mr-2 hover:bg-blue-700">
                         <i className="fas fa-paperclip"></i> {/* Attachment icon */}
@@ -24,7 +25,7 @@ function HomePage() {
                     <button className="bg-gray-300 rounded-full p-2 mx-2">Text to Video</button>
                 </div>
             </div>
-            <div className="w-1/2 p-10">
+            <div className="w-[60%] p-2">
                 <div className="border border-gray-300 rounded-lg h-full">
                     {/* Images will be displayed here */}
                 </div>
